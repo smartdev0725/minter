12a6cb93c865fdf31a7202770ccdc82f9e8e9b3b
smartdev0725 <cetbdb23@gmail.com>
2020-12-10 16:14:12 +0800

    initial test outline, hardhat remove fork, package log

---
+++ test/Minter.ts
@@ -0,0 +1,14 @@
+import { ethers } from 'hardhat'
+import { expect } from 'chai'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+// import {  } from '../typechain/Token'
+
+describe('Can mint synthetic', () => {
+  beforeEach(async () => {})
+  describe('Can accept DAI collateral', () => {})
+  describe('Synthetic is minted', () => {})
+  describe('Can transfer synth to recipient wallet', () => {})
+  describe('Can redeem synth for DAI collateral', () => {})
+  describe('Can earn HALO upon synth mint', () => {})
+  describe('Can earn HALO on transfer to whitelisted AMM address', () => {})
+})
